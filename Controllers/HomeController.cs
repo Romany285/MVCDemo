8676c952de225eb2acb5f9078703ccb9fb9a0a44
romany <romanyghttas332@gmail.com>
2024-10-05 06:29:48 +0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "this is index action";
+        }
+        public string AboutUs()
+        {
+            return "this is About Us action";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+namespace MVCDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllers();
+            var app = builder.Build();
+            app.UseRouting();
+            #region MyRegion
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapGet("/home", async context =>
+            //    {
+            //        await context.Response.WriteAsync("Hello World! => home");
+            //    });
+            //    //endpoints.MapGet("/products", async context =>
+            //    //{
+            //    //    await context.Response.WriteAsync("Hello World! => products");
+            //    //});
+            //    endpoints.MapGet("/products/{id:int?}", async context =>
+            //    {
+            //        var idData = context.Request.RouteValues["id"];
+            //        if (idData is not null)
+            //        {
+            //            int data = Convert.ToInt32(idData);
+            //            await context.Response.WriteAsync($"Hello World! => products id => {data}");
+            //        }
+            //        else
+            //        {
+            //            await context.Response.WriteAsync("you are at product page");
+            //        }
+
+            //    });
+            //    // parameters
+            //    endpoints.MapGet("/books/{id}/{Auther:alpha:minlength(5):maxlength(20)}", async context =>
+            //    {
+            //        int id = Convert.ToInt32(context.Request.RouteValues["id"]);
+            //        string name = context.Request.RouteValues["Auther"].ToString();
+            //        await context.Response.WriteAsync($"Hello World! => products {id} , {name}");
+            //    });
+            //});
+            #endregion
+            app.MapControllerRoute
+                (
+                    name : "default",
+                    pattern : "/{Controller = Home}/{Action = Index}",
+                    defaults : new { Controller = "Home" , Action = "AboutUs" }
+                );
+
+            //app.MapGet("/", () => "Hello World!");
+            //app.Run(async (HttpContent) => { await HttpContent.Response.WriteAsync("hello"); });
+            app.Run();
+        }
+    }
+}
